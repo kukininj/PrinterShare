c373598fecd788ad081b00adf43e2925d1d64f9b
Łukasz Niedzielski <luk20001122@gmail.com>
2023-02-09 00:03:54 +0100

    `UserRepository` repository, `User` model

---
+++ src/models/User.php
@@ -0,0 +1,35 @@
+<?php
+
+class User
+{
+    public int $id_user;
+    public string $email;
+    public string $password_hash;
+    public string $name;
+    public string $surname;
+    public string $profile_picture;
+    public ?int $id_merchant;
+    public ?string $area_name;
+
+
+    public function __construct(
+        int $id_user,
+        string $email,
+        string $password_hash,
+        string $name,
+        string $surname,
+        string $profile_picture,
+        ?int $id_merchant,
+        ?string $area_name
+    ) {
+        $this->id_user = $id_user;
+        $this->email = $email;
+        $this->password_hash = $password_hash;
+        $this->name = $name;
+        $this->surname = $surname;
+        $this->profile_picture = $profile_picture;
+        $this->id_merchant = $id_merchant;
+        $this->area_name = $area_name;
+    }
+
+}
---
+++ src/repositories/UserRepository.php
@@ -0,0 +1,142 @@
+<?php
+
+require_once('Repository.php');
+require_once(__DIR__ . '/../models/User.php');
+
+class UserRepository extends Repository
+{
+    /**
+     * @var array<int, User> $users
+     */
+    private static array $users = [];
+
+    public static function &getUserByEmail(string $email): ?User
+    {
+        $statement = self::database()->connect()->prepare("
+            SELECT users.*, merchant_details.*, dedicated_areas.area_name
+            FROM users
+            JOIN merchant_details ON users.ID_merchant = merchant_details.ID_merchant
+            JOIN dedicated_areas ON merchant_details.ID_dedicated_area = dedicated_areas.ID_dedicated_area;
+        ");
+
+        $statement->bindParam('email', $email, PDO::PARAM_STR);
+        $statement->execute();
+
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+        $user = new User(
+            $result['ID_user'],
+            $result['email'],
+            $result['password'],
+            $result['name'],
+            $result['surname'],
+            $result['profile_picture'],
+            $result['ID_merchant'],
+            $result['area_name']
+        );
+
+        if (array_key_exists($user->id_user, self::$users)) {
+            return self::$users[$user->id_user];
+        } else {
+            self::$users[$user->id_user] = $user;
+            return $user;
+        }
+    }
+
+    public static function &addUser(
+        string $email,
+        string $password_hash,
+        string $name,
+        string $surname,
+        string $profile_picture,
+    ): User {
+        $statement = self::database()->connect()->prepare("
+            INSERT INTO users (email, password, name, surname, profile_picture)
+            VALUES (:email, :password, :name, :surname, :profile_picture)
+            RETURNING users.*;
+        ");
+
+        $statement->bindParam('email', $email, PDO::PARAM_STR);
+        $statement->bindParam('password', $password_hash, PDO::PARAM_STR);
+        $statement->bindParam('name', $name, PDO::PARAM_STR);
+        $statement->bindParam('surname', $surname, PDO::PARAM_STR);
+        $statement->bindParam('profile_picture', $profile_picture, PDO::PARAM_STR);
+        $statement->execute();
+
+        $user_data = $statement->fetch(PDO::FETCH_ASSOC);
+
+        $user = new User(
+            $user_data['ID_user'],
+            $user_data['email'],
+            $user_data['password'],
+            $user_data['name'],
+            $user_data['surname'],
+            $user_data['profile_picture'],
+            null,
+            null
+        );
+
+        if (array_key_exists($user->id_user, self::$users))
+            fwrite(STDOUT, "user " . $user->id_user . " already in storage, overriding...");
+
+        self::$users[$user->id_user] = $user;
+
+        return $user;
+    }
+
+    public static function &addMerchant(
+        string $email,
+        string $password_hash,
+        string $name,
+        string $surname,
+        string $profile_picture,
+        string $area,
+    ): User {
+        $statement = self::database()->connect()->prepare("
+        INSERT INTO merchant_details (\"ID_dedicated_area\")
+        SELECT \"ID_dedicated_area\" FROM dedicated_areas WHERE area_name=:area_name LIMIT 1
+        RETURNING merchant_details.\"ID_merchant\";
+        ");
+
+        $statement->bindParam('area_name', $area, PDO::PARAM_STR);
+        $statement->execute();
+
+        $user_data = $statement->fetch(PDO::FETCH_ASSOC);
+        $id_merchant = $user_data['ID_merchant'] ?? throw new PDOException("invalid area_name");
+
+        
+        $statement = self::database()->connect()->prepare("
+            INSERT INTO users (email, password, \"ID_merchant\", name, surname, profile_picture)
+            VALUES (:email, :password, :ID_merchant, :name, :surname, :profile_picture)
+            RETURNING users.*;
+        ");
+
+        $statement->bindParam('email', $email, PDO::PARAM_STR);
+        $statement->bindParam('password', $password_hash, PDO::PARAM_STR);
+        $statement->bindParam('name', $name, PDO::PARAM_STR);
+        $statement->bindParam('surname', $surname, PDO::PARAM_STR);
+        $statement->bindParam('profile_picture', $profile_picture, PDO::PARAM_STR);
+        $statement->bindParam('ID_merchant', $id_merchant, PDO::PARAM_INT);
+        $statement->execute();
+
+        $user_data = $statement->fetch(PDO::FETCH_ASSOC);
+
+        $user = new User(
+            $user_data['ID_user'],
+            $user_data['email'],
+            $user_data['password'],
+            $user_data['name'],
+            $user_data['surname'],
+            $user_data['profile_picture'],
+            $user_data['ID_merchant'],
+            $area,
+        );
+
+        if (array_key_exists($user->id_user, self::$users))
+            fwrite(STDOUT, "user " . $user->id_user . " already in storage, overriding...");
+
+        self::$users[$user->id_user] = $user;
+
+        return $user;
+    }
+}
